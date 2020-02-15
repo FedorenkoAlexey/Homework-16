c9de55c40c31471137bbae8eb52088425c6194e4
Alexey Fedorenko <messer.ua@gmail.com>
2020-02-15 14:16:23 +0200

    add form field

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl } from "@angular/forms";
+import { Component, OnInit, InjectionToken } from "@angular/core";
+import { FormControl, FormGroup, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: "app-reactive-form",
@@ -7,10 +7,44 @@ import { FormControl } from "@angular/forms";
   styleUrls: ["./reactive-form.css"]
 })
 export class ReactiveFormComponent implements OnInit {
-  constructor() {}
-  firstNameControl: FormControl;
+  MAT_INPUT_VALUE_ACCESSOR: InjectionToken<{ value: any }>;
+  // constructor(private fb: FormBuilder) {}
+  reactiveForm: FormGroup;
+
+  // reactiveForm = this.fb.group({
+  //   userName: [],
+  //   firstName: [],
+  //   lastName: [],
+  //   email: []
+  // });
+
+  onSubmit() {
+    console.warn(this.reactiveForm.value);
+  }
 
   ngOnInit() {
-    this.firstNameControl = new FormControl();
+    // this.firstNameControl = new FormControl();
+    // this.firstNameControl.valueChanges.subscribe(() =>
+    //   console.log(this.firstNameControl)
+    // );
+
+    // this.reactiveForm = new FormGroup({
+    //   userName: new FormControl("user"),
+    //   firstName: new FormControl("F-N"),
+    //   lastName: new FormControl("L-N")
+    // });
+
+    this.reactiveForm = new FormGroup({
+      user: new FormGroup({
+        userName: new FormControl("user"),
+        firstName: new FormControl("F-N"),
+        lastName: new FormControl("L-N")
+      }),
+      email: new FormControl(""),
+      pass: new FormGroup({
+        password: new FormControl(""),
+        repeatPass: new FormControl("")
+      })
+    });
   }
 }
